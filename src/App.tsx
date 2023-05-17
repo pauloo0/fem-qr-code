ebfe46756f5e9ca5b0a960d1379eac9c2e46056e
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2023-05-17 22:37:52 +0100

    Adding HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 function App() {
-  return <div className='grid place-items-center'>Hello World</div>
+  return (
+    <div className='min-h-screen bg-light_gray grid place-items-center'>
+      <main>
+        <section>
+          <img src='./assets/images/image-qr-code.png' alt='QR Code' />
+        </section>
+        <section>
+          <h1>Improve your front-end skills by building projects</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </section>
+      </main>
+    </div>
+  )
 }
 
 export default App
